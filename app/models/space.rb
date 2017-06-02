f8a8faf82b4b84efa14bb966236985f380538690
kojiro3 <kojiro@aokikoujirou-no-MacBook-Air.local>
2017-06-02 15:40:58 +0900

    Edit_spaces_index_view

--- app/models/space.rb
+++ app/models/space.rb
@@ -8,4 +8,12 @@ class Space < ApplicationRecord
   validates :text, length: { in: 10..800 }
   validates :postal_code, length: { is: 7 }, numericality: :only_integer
   validates :price, :number, numericality: true
+
+  scope :party, -> { where(purpose: 'パーティー') }
+  scope :meeting, -> { where(purpose: '会議') }
+  scope :photo, -> { where(purpose: '写真撮影') }
+  scope :location, -> { where(purpose: 'ロケ撮影') }
+  scope :event, -> { where(purpose: 'イベント') }
+  scope :stay, -> { where(purpose: '宿泊') }
+  scope :other, -> { where(purpose: 'その他') }
 end
