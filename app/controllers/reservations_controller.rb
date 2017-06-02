35d870604e0d450230d18a9b46d19022fe7a47c2
kojiro3 <kojiro@aokikoujirou-no-MacBook-Air.local>
2017-06-02 14:48:31 +0900

    Add_create_resevation_function

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -4,4 +4,20 @@ class ReservationsController < ApplicationController
     @reservation = Reservation.new
     @space = Space.find(params[:space_id])
   end
+
+  def create
+    @reservation = current_user.reservations.new(reservation_params)
+    @space = Space.find(params[:space_id])
+    if @reservation.save
+    else
+      flash.now[:alert] = 'スペースの予約に失敗しました'
+      render :new
+    end
+  end
+
+  private
+
+  def reservation_params
+    params.permit(:year, :month, :day, :start, :finish).merge(space_id: params[:space_id])
+  end
 end
